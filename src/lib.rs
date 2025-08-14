14fb82f687c76c8604cf210b7ff1b5de2f292f94
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2025-08-14 09:25:41 -0400

    Compiles

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,230 @@
 mod utils;
 
 use wasm_bindgen::prelude::*;
+use wgpu::{TextureDescriptor, TextureUsages, TextureFormat, Extent3d};
 
 #[wasm_bindgen]
 extern "C" {
     fn alert(s: &str);
 }
 
+// This function should accept width and height as parameters,
+// and return a Uint8Array containing the rendered image data.
+#[wasm_bindgen]
+pub async fn render(width: u32, height: u32) {
+    // The Instance is the context for all other wgpu objects.
+    // This is the first thing you create when using wgpu.
+    // Its primary use is to create Adapters and Surfaces.
+    // Does not have to be kept alive.
+    
+    // The InstanceDescriptor has fields for which backends wgpu will choose during instantiation,
+    // and which DX12 shader compiler wgpu will use.
+    let instance = wgpu::Instance::new(&wgpu::InstanceDescriptor::default());
+    let adapter = instance
+        .request_adapter(&wgpu::RequestAdapterOptions::default())
+        .await
+        .expect("No suitable GPU adapters found on the system!");
+    
+    // Requests a connection to a physical device, creating a logical device.
+    // Returns the Device together with a Queue that executes command buffers.
+    // Per the WebGPU specification, an Adapter may only be used once to create a device.
+    // If another device is wanted, call Instance::request_adapter() again to get a fresh Adapter.
+    // However, wgpu does not currently enforce this restriction.
+    let (device, queue) = adapter
+        .request_device(&wgpu::DeviceDescriptor::default())
+        .await
+        .expect("Failed to create device");
+    
+    // Create a texture to render to.
+    let texture_desc = TextureDescriptor {
+        // Debug label of the texture. This will show up in graphics debuggers for easy identification.
+        label: Some("Render Texture"),
+        // Size of the texture. All components must be greater than zero.
+        // For a regular 1D/2D texture, the unused sizes will be 1.
+        // For 2DArray textures, Z is the number of 2D textures in that array.
+        size: Extent3d {
+            width,
+            height,
+            depth_or_array_layers: 1,
+        },
+        // Mip count of texture. For a texture with no extra mips, this must be 1.
+        mip_level_count: 1,
+        // Sample count of texture. If this is not 1, texture must have [BindingType::Texture::multisampled] set to true.
+        sample_count: 1,
+        // Dimensions of the texture.
+        dimension: wgpu::TextureDimension::D2,
+        // Format of the texture.
+        format: TextureFormat::Rgba8UnormSrgb,
+        // Allowed usages of the texture. If used in other ways, the operation will panic.
+        usage: TextureUsages::RENDER_ATTACHMENT | TextureUsages::COPY_SRC,
+        // Specifies what view formats will be allowed when calling Texture::create_view on this texture.
+        // View formats of the same format as the texture are always allowed.
+        // Note: currently, only the srgb-ness is allowed to change. (ex: Rgba8Unorm texture + Rgba8UnormSrgb view)
+        view_formats: &[],
+    };
+    let texture = device.create_texture(&texture_desc);
+    
+    // Creates a view of this texture,
+    // specifying an interpretation of its texels and possibly a subset of its layers and mip levels.
+    // Texture views are needed to use a texture as a binding in a BindGroup
+    // or as an attachment in a RenderPass.
+    let view = texture.create_view(&Default::default());
+
+    // Create a buffer to store the output
+    let u32_size = std::mem::size_of::<u32>() as u32;
+    let output_buffer_size = (u32_size * width * height) as wgpu::BufferAddress;
+    let output_buffer_desc = wgpu::BufferDescriptor {
+        size: output_buffer_size,
+        usage: wgpu::BufferUsages::COPY_DST | wgpu::BufferUsages::MAP_READ,
+        label: Some("Output Buffer"),
+        mapped_at_creation: false,
+    };
+    let output_buffer = device.create_buffer(&output_buffer_desc);
+
+    let vs_src = r#"
+        @vertex
+        fn vs_main(@builtin(vertex_index) in_vertex_index: u32) -> @builtin(position) vec4<f32> {
+            let x = f32(i32(in_vertex_index) - 1);
+            let y = f32(i32(in_vertex_index & 1u) * 2 - 1);
+            return vec4<f32>(x, y, 0.0, 1.0);
+        }
+    "#;
+
+    let fs_src = r#"
+        @fragment
+        fn fs_main() -> @location(0) vec4<f32> {
+            return vec4<f32>(1.0, 0.0, 0.0, 1.0);
+        }
+    "#;
+
+    let vs_module = device.create_shader_module(wgpu::ShaderModuleDescriptor {
+        label: Some("Vertex Shader"),
+        source: wgpu::ShaderSource::Wgsl(vs_src.into()),
+    });
+
+    let fs_module = device.create_shader_module(wgpu::ShaderModuleDescriptor {
+        label: Some("Fragment Shader"),
+        source: wgpu::ShaderSource::Wgsl(fs_src.into()),
+    });
+
+    let render_pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+        label: Some("Render Pipeline Layout"),
+        bind_group_layouts: &[],
+        push_constant_ranges: &[],
+    });
+
+    let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+        label: Some("Render Pipeline"),
+        layout: Some(&render_pipeline_layout),
+        vertex: wgpu::VertexState {
+            module: &vs_module,
+            entry_point: Some("vs_main"),
+            compilation_options: Default::default(),
+            buffers: &[],
+        },
+        fragment: Some(wgpu::FragmentState {
+            module: &fs_module,
+            entry_point: Some("fs_main"),
+            compilation_options: Default::default(),
+            targets: &[Some(wgpu::ColorTargetState {
+                format: texture_desc.format,
+                blend: Some(wgpu::BlendState::REPLACE),
+                write_mask: wgpu::ColorWrites::ALL,
+            })],
+        }),
+        primitive: wgpu::PrimitiveState {
+            topology: wgpu::PrimitiveTopology::TriangleList,
+            ..Default::default()
+        },
+        depth_stencil: None,
+        multisample: wgpu::MultisampleState::default(),
+        multiview: None,
+        cache: None,
+    });
+
+    let mut encoder = device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
+        label: Some("Render Encoder"),
+    });
+
+    {
+        let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+            label: Some("Render Pass"),
+            color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                view: &view,
+                depth_slice: None,
+                resolve_target: None,
+                ops: wgpu::Operations {
+                    load: wgpu::LoadOp::Clear(wgpu::Color {
+                        r: 0.1,
+                        g: 0.2,
+                        b: 0.3,
+                        a: 1.0,
+                    }),
+                    store: wgpu::StoreOp::Store,
+                },
+            })],
+            depth_stencil_attachment: None,
+            timestamp_writes: None,
+            occlusion_query_set: None,
+        });
+
+        render_pass.set_pipeline(&render_pipeline);
+        render_pass.draw(0..3, 0..1);
+    }
+
+    encoder.copy_texture_to_buffer(
+        wgpu::TexelCopyTextureInfo {
+            texture: &texture,
+            mip_level: 0,
+            origin: wgpu::Origin3d::ZERO,
+            aspect: wgpu::TextureAspect::All,
+        },
+        wgpu::TexelCopyBufferInfo {
+            buffer: &output_buffer,
+            layout: wgpu::TexelCopyBufferLayout {
+                offset: 0,
+                bytes_per_row: Some(u32_size * width),
+                rows_per_image: Some(height),
+            },
+        },
+        texture_desc.size,
+    );
+
+    // We finish the encoder, giving us a fully recorded command buffer.
+    let command_buffer = encoder.finish();
+
+    // At this point nothing has actually been executed on the gpu. We have recorded a series of
+    // commands that we want to execute, but they haven't been sent to the gpu yet.
+    //
+    // Submitting to the queue sends the command buffer to the gpu. The gpu will then execute the
+    // commands in the command buffer in order.
+    queue.submit([command_buffer]);
+
+    // We now map the output buffer so we can read it. Mapping tells wgpu that we want to read/write
+    // to the buffer directly by the CPU and it should not permit any more GPU operations on the buffer.
+    //
+    // Mapping requires that the GPU be finished using the buffer before it resolves, so mapping has a callback
+    // to tell you when the mapping is complete.
+    let buffer_slice = output_buffer.slice(..);
+    buffer_slice.map_async(wgpu::MapMode::Read, |_| {
+        // In this case we know exactly when the mapping will be finished,
+        // so we don't need to do anything in the callback.
+    });
+
+    // Wait for the GPU to finish working on the submitted work. This doesn't work on WebGPU, so we would need
+    // to rely on the callback to know when the buffer is mapped.
+    device.poll(wgpu::PollType::Wait).unwrap();
+
+    // We can now read the data from the buffer.
+    let data = buffer_slice.get_mapped_range();
+    // Convert the data back to a slice of f32.
+    let result: &[f32] = bytemuck::cast_slice(&data);
+
+    // Print out the result.
+    println!("Result: {result:?}");
+}
+
 #[wasm_bindgen]
 pub fn greet() {
-    alert("Hello, wasm-game-of-life!");
+    alert("Hello, pluot!");
 }
