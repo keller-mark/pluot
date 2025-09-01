8a35ec2f965fa6878118321644a4efe71ef9b09a
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2025-09-01 16:02:55 -0400

    Update zarrs hash

--- src/bindings.rs
+++ src/bindings.rs
@@ -70,7 +70,6 @@ pub mod wasm {
         //
         // For more details see
         // https://github.com/rustwasm/console_error_panic_hook#readme
-        #[cfg(feature = "console_error_panic_hook")]
         console_error_panic_hook::set_once();
     }
 
