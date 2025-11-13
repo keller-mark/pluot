292b6633cb42bed75d78e0a1581882ff72ff2d45
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2025-11-12 21:54:37 -0500

    Support passing _arr=numpy_arr via plot_params when store_name is missing

--- python/pluot/zarr.py
+++ python/pluot/zarr.py
@@ -1,6 +1,7 @@
 import zarr
 from zarr.storage import MemoryStore, LocalStore
 from zarr.abc.store import RangeByteRequest, SuffixByteRequest
+from zarr.core.buffer.core import default_buffer_prototype 
 from os.path import join, dirname
 
 # Global mapping from store_name to Zarr store objects.
@@ -20,16 +21,27 @@ async def zarr_get(store_name: str, key: str) -> bytes:
     """Get the value for a key from the Zarr store."""
     store = GLOBAL_STORES[store_name]
     #print(f"Getting key '{key}' from store '{store_name}'")
-    return (await store.get(key)).to_bytes()
+    return (await store.get(
+        key,
+        prototype=default_buffer_prototype(),
+    )).to_bytes()
 
 async def zarr_get_range_from_offset(store_name: str, key: str, offset: int, length: int) -> bytes:
     """Get a byte range from a value in the Zarr store, specified by offset and length."""
     store = GLOBAL_STORES[store_name]
     #print(f"Getting range from offset {offset} with length {length} for key '{key}' from store '{store_name}'")
-    return (await store.get(key, byte_range=RangeByteRequest(start=offset, end=offset+length))).to_bytes()
+    return (await store.get(
+        key,
+        byte_range=RangeByteRequest(start=offset, end=offset+length),
+        prototype=default_buffer_prototype(),
+    )).to_bytes()
 
 async def zarr_get_range_from_end(store_name: str, key: str, suffix_length: int) -> bytes:
     """Get a byte range from the end of a value in the Zarr store, specified by length."""
     store = GLOBAL_STORES[store_name]
     #print(f"Getting range from end with length {suffix_length} for key '{key}' from store '{store_name}'")
-    return (await store.get(key, byte_range=SuffixByteRequest(suffix=suffix_length))).to_bytes()
+    return (await store.get(
+        key,
+        byte_range=SuffixByteRequest(suffix=suffix_length),
+        prototype=default_buffer_prototype(),
+    )).to_bytes()
