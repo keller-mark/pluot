72996e6752ab471886899c8ed6c35e0a5e3373c0
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2026-01-07 15:38:14 -0500

    WIP: render_svg and render_canvas with Draw traits

--- src/deckish/layer.rs
+++ src/deckish/layer.rs
@@ -1,42 +1,82 @@
-struct ViewParams {
-  width
-  height
-  camera_view
-  margin_left
-  margin_right
-  margin_top
-  margin_bottom
-  timeout
-  cache_enabled
-  device_pixel_ratio
-  // ... anything else at the view level (not layer-specific)
+use crate::wgpu;
+use crate::two::svg::{init_svg};
+use svg::node::element::Group;
+use crate::params::{RenderContext, RenderResult};
+
+// Struct to store anything at the view level (i.e., not layer-specific)
+pub struct ViewParams {
+    pub width: u32,
+    pub height: u32,
+
+    // Device pixel ratio to support retina displays.
+    // Default to 1.0 for standard displays.
+    // Retina screens will have a value of 2.0 or higher.
+    pub device_pixel_ratio: f32,
+
+    pub camera_view: Option<[f32; 16]>,
+
+    // Timeout in ms before bailing out of awaiting a data request.
+    pub timeout: Option<u32>,
+
+    // Allow disabling memoization/cacheing. Useful for testing/debugging.
+    pub cache_enabled: bool,
+
+    // Margins for plots that need them (e.g. scatterplot axes).
+    pub margin_left: Option<f32>,
+    pub margin_right: Option<f32>,
+    pub margin_top: Option<f32>,
+    pub margin_bottom: Option<f32>,
 }
 
-struct ScatterplotLayer {
+impl Default for ViewParams {
+    fn default() -> Self {
+        Self {
+            width: 100,
+            height: 100,
+            device_pixel_ratio: 1.0,
+            camera_view: None,
+            timeout: None,
+            cache_enabled: true,
+            margin_left: None,
+            margin_right: None,
+            margin_top: None,
+            margin_bottom: None,
+        }
+    }
 }
 
-impl DrawToSvg for ScatterplotLayer {
-   async fn draw(self, ) -> SvgNode {
-
-   }
+#[cfg_attr(target_arch = "wasm32", async_trait::async_trait(?Send))]
+#[cfg_attr(not(target_arch = "wasm32"), async_trait::async_trait)]
+pub trait DrawToSvg {
+    async fn draw(&self, group: &Group) -> Group;
 }
 
-impl DrawToCanvas for ScatterplotLayer {
-  async fn get_model(self, device, queue) {
-     // use memoization
-  }
-  async fn draw(self, device, queue, encoder) {
-    // create pass from encoder
-    let model = self.get_model(device, queue);
-    model.draw(pass)
-    // finish render pass
-  }
+#[cfg_attr(target_arch = "wasm32", async_trait::async_trait(?Send))]
+#[cfg_attr(not(target_arch = "wasm32"), async_trait::async_trait)]
+pub trait DrawToCanvas {
+    async fn draw(&self, device: wgpu::Device, queue: wgpu::Queue, encoder: &wgpu::CommandEncoder);
 }
 
-async fn render_svg(layers: Vec<Layer>, view_params: ViewParams) {
 
+pub async fn render_svg(view_params: ViewParams, layers: Vec<Box<dyn DrawToSvg>>) -> Group {
+    let (_, group) = init_svg(view_params.width as f64, view_params.height as f64);
+    let mut group = group;
+    for layer in &layers {
+        // TODO: when/where to pass view_params to each layer?
+        group = layer.draw(&group).await;
+    }
+
+    // TODO: also return RenderResult? How to aggregate results from multiple layers?
+
+    group
 }
 
-async fn render_canvas(layers: Vec<Layer>, view_params: ViewParams) {
+pub async fn render_canvas(view_params: ViewParams, layers: Vec<Box<dyn DrawToCanvas>>, context: &mut RenderContext<'_>, encoder: &mut wgpu::CommandEncoder) {
+    for layer in &layers {
+        // TODO: when/where to pass view_params to each layer? during draw call? before draw call?
+        // Should we instead assume the layer already has the necessary info from view_params?
+        layer.draw(context.device.clone(), context.queue.clone(), encoder).await;
+    }
 
+    // TODO: return RenderResult? How to aggregate results from multiple layers?
 }
