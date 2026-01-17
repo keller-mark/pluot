b06c6e3e82d704aafd7af9f8b49ee4203b150eeb
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2026-01-17 18:21:25 -0500

    Update

--- js/docs/src/components/Another.jsx
+++ js/docs/src/components/Another.jsx
@@ -1,18 +1,34 @@
 import React, { useState, useMemo, lazy, Suspense } from 'react';
 
+
 const Pluot = lazy(async () => {
-    return (await import('pluot-wrapper')).Pluot;
+    // For 3d-view-controls.
+    window.global = window;
+    return {
+        default: (await import('pluot-wrapper')).Pluot,
+    };
 });
 
-console.log(Pluot)
+
 export function Another(props) {
     
-
-    
-
     return (
         <Suspense fallback={<p>Loading Pluot...</p>}>
-            <Pluot />
+            <Pluot
+                width={500}
+                height={500}
+                plotId={"docs-example-scatterplot"}
+                plotType={"LayeredPlot"}
+                // TODO: host the store somewhere remotely.
+                storeName={"gaussian_quantiles_store"}
+                plotParams={{
+                    x_key: "/n_1000000/x_coords",
+                    y_key: "/n_1000000/y_coords",
+                    color_key: "/n_1000000/class_labels",
+                    point_radius: 5.0,
+                }}
+                mode={"2d"}
+            />
         </Suspense>
     );
 }
