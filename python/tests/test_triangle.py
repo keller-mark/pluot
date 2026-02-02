b4154f133dd4d9dd44d07f57ff17c62e65acd95d
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2026-02-02 13:48:23 -0500

    Update python test

--- python/tests/test_triangle.py
+++ python/tests/test_triangle.py
@@ -2,18 +2,52 @@ import pytest
 
 from pluot import render, render_to_array
 
+camera_view = [
+    1.0, 0.0, 0.0, 0.0,
+    0.0, 1.0, 0.0, 0.0,
+    0.0, 0.0, 1.0, 0.0,
+    0.0, 0.0, 0.0, 1.0,
+]
+
+basic_plot_kwargs = dict(
+    camera_view=camera_view,
+    width=100, 
+    height=100, 
+    plot_id="test",
+    plot_type="LayeredPlot",
+    store_name="my_store",
+    plot_params=dict(
+        layers=[
+            dict(
+                layer_type="ScatterplotLayer",
+                    layer_params=dict(
+                    layer_id="scatter_layer",
+                    data_unit_mode="Data",
+                    point_radius_unit_mode="Pixels",
+                    point_shape_mode="Square",
+                    point_radius=25.0,
+                    bounds=None,
+                    x_vec=[0, 1, 0, 1],
+                    y_vec=[0, 0, 1, 1],
+                    labels_vec=[0, 1, 2, 3],
+                )
+            ),
+        ]
+    ),
+)
+
 @pytest.mark.asyncio
 async def test_render_triangle():
-    result = await render(width=100, height=100, plot_id="test", plot_type="Triangle", store_name="test")
+    result = await render(**basic_plot_kwargs)
     assert result is not None
     assert len(result) == (100 * 100 * 4) + 1  # RGBA for each pixel, plus one extra value
-    assert sum(result) == 5100000  # Expected sum for a triangle rendering
+    assert sum(result) == 9475000  # Expected sum for a triangle rendering
 
 @pytest.mark.asyncio
 async def test_render_to_array():
-    arr = await render_to_array(width=100, height=200, plot_id="test", plot_type="Triangle", store_name="test")
-    assert arr.shape == (200, 100, 4)
+    arr = await render_to_array(**basic_plot_kwargs)
+    assert arr.shape == (100, 100, 4)
     assert arr.dtype == 'uint8'
-    assert arr.sum() == 10200000
+    assert arr.sum() == 9475000
 
 # TODO: test that a render call that internally uses the Vello renderer can be run twice in a row successfully.
