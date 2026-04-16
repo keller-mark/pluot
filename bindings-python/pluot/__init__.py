6de8a1deadeff4e61c729392d41826f05ea2c39f
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2026-04-16 15:21:00 -0400

    Add svg rendering function to python package

--- bindings-python/pluot/__init__.py
+++ bindings-python/pluot/__init__.py
@@ -1,5 +1,5 @@
 
-from .render import render, render_to_array, render_to_image
+from .render import render, render_to_array, render_to_image, render_to_svg
 from .log import get_logger
 import logging
 # TODO: remove this, leave to user to configure.
