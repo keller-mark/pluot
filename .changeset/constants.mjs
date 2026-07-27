ea3a1dcdf4c54c0508511373b2fb91044bf51228
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2026-07-27 10:15:06 -0400

    Release workflows

---
+++ .changeset/constants.mjs
@@ -0,0 +1,4 @@
+import { join } from 'node:path';
+
+// The path to the 'pluot' package from the root of the repo.
+export const MAIN_PACKAGE_DIR = join('crates', 'pluot', 'pkg');
---
+++ scripts/changeset-publish.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+set -o errexit
+set -o pipefail
+
+if [[ "$1" != "--action" ]]; then
+  if ! command -v gh &> /dev/null
+  then
+      echo "Requires the GitHub CLI (gh)." >&2
+      exit 1
+  fi
+fi
+
+# This is so that we can have nice release notes like https://github.com/d3/d3/releases in GitHub releases.
+# The RELEASE_NOTES.md file is temporarily created during CI in order to define the release notes,
+# but is not intended to be committed to the repo.
+
+# Temporarily copy the root README.md to crates/pluot/pkg/README.md,
+# since the file at crates/pluot/pkg will be shown for the main 'pluot'
+# package on NPM.
+#mv ./crates/pluot/pkg/README.md ./crates/pluot/pkg/README.temp.md
+#cp ./README.md ./crates/pluot/pkg/README.md
+
+# Publish all sub-packages to NPM.
+pnpm publish --filter='./bindings-js/**' --filter='./crates/pluot/pkg' --no-git-checks --provenance --access public
+
+# TODO: Publish python package to PyPI, but only when not --action?
+
+# Revert the copied README
+#rm ./packages/main/prod/README.md
+#mv ./packages/main/prod/README.temp.md ./packages/main/prod/README.md
+
+# Tag this as the release commit.
+git tag v$(cat ./package.json | jq -r .version)
+# Push the tag to GitHub.
+git push --follow-tags
+
+# Create RELEASE_NOTES.md from the latest entries in CHANGELOG.md.
+node .changeset/get-release-notes.mjs
+
+# Either indicate to softprops/action-gh-release that there should be a release,
+# or create a release from the command line.
+if [[ "$1" == "--action" ]]; then
+  echo "should-release=true" >> $GITHUB_OUTPUT
+else
+  # Need to create a GitHub release from the command line.
+  TAG_NAME=v$(cat ./package.json | jq -r .version)
+  # Use the GitHub CLI to create a release.
+  gh release create $TAG_NAME -F RELEASE_NOTES.md
+fi
