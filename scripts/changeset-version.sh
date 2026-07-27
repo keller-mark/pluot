80222572164bb1d114bdd8f56dff8d13f66a1c5c
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2026-07-27 10:59:36 -0400

    Updates

--- scripts/changeset-version.sh
+++ scripts/changeset-version.sh
@@ -13,6 +13,10 @@ fi
 pnpm exec changeset version
 # Note: this will not immediately affect crates/pluot/pkg/package.json, as this version matches the version in crates/pluot/Cargo.toml
 
+node .changeset/post-changelog.mjs
+
+# The above post-changelog script modifies the version of the root package.json file,
+# so we obtain the version following that script.
 NEXT_VERSION=$(cat ./package.json | jq -r .version)
 
 # Set rust crate versions using cargo-edit
@@ -23,5 +27,3 @@ cargo set-version --manifest-path examples/pluot_cli/Cargo.toml $NEXT_VERSION
 perl -pi -e "s/^Version: .*/Version: ${NEXT_VERSION}/" bindings-r/DESCRIPTION
 
 # Note: python package version is already dynamic.
-
-node .changeset/post-changelog.mjs
