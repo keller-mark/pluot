4174a2fa1b50d5d251c7cc6a48c5fd2e8be20b7f
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2026-08-26 20:41:24 -0400

    TODO

---
+++ bindings-js/react/src/Brush.jsx
@@ -0,0 +1,5 @@
+// TODO: implement a react component that allows a user to click and drag to create a 1D (along X or Y only) or 2D (rectangular) brush selection.
+// Accept the width, height, marginLeft, marginRight, marginTop, marginBottom as props
+// Accept a callback that enables emitting the brushed rectangle coordinates/dimensions to a parent component
+// Accept a prop which determines whether the brush is 1D/2D and whether the brush is allowed in the full width/height, only in the layer bounds (within the margins), or only within a specified margin (l/r/b/t).
+// Do not use D3-brush - write your own event handlers and use react idioms
